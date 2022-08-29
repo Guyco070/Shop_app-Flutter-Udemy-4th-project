8cb2a6a1afa282827e26678750df1293431d15b0
Guy Cohen <gaico070@gmail.com>
2022-08-29 16:03:45 +0300

    section 10

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -41,3 +41,59 @@ class OrdersScreen extends StatelessWidget {
         ));
   }
 }
+
+/*
+// using FutureBuilder without rerun the Future method. (at other cases)
+class OrdersScreen extends StatefulWidget {
+  const OrdersScreen({Key? key}) : super(key: key);
+
+  static String routeName = '/orders_screen';
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  late Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Your Orders'),
+        ),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+          future:
+              _ordersFuture,
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (dataSnapshot.error != null) {
+              return const Center(
+                child: Text('An error Ocurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                  builder: ((context, ordersData, child) => ListView.builder(
+                        itemBuilder: ((context, index) =>
+                            OrderItem(order: ordersData.orders[index])),
+                        itemCount: ordersData.orders.length,
+                      )));
+            }
+          },
+        ));
+  }
+}
+*/
